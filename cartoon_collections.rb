ae5120a7bfa060471fa2f1cd783115a4884c0cfd
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-29 08:46:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+array.each_with_index do |value, index|
+  puts "#{index+1}.#{value}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+ array.map do |call|
+   "#{call.capitalize + '!'}"
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.reduce do |call|
+     return true if call.length > 4
+   end
+   return false
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
